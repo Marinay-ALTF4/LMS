8ff2b791391d36d21628fe193cfb62a7e03e9f82
Marinay-ALTF4 <adriennemarinay@gmail.com>
2026-03-14 10:20:55 +0800

    Update configuration files and improve documentation

--- system/API/ResponseTrait.php
+++ system/API/ResponseTrait.php
@@ -3,7 +3,7 @@
 declare(strict_types=1);
 
 /**
- * This file is part of CodeIgniter 4 framework.
+ * This file is part of CodeIgniter 4 frameworks.
  *
  * (c) CodeIgniter Foundation <admin@codeigniter.com>
  *
